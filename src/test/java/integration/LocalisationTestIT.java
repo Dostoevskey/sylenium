d58fcc305dd9aedc6573c26c4594e2eaeb862c79
simonk <sek_99@hotmail.co.uk>
2018-10-19 22:15:01 +0100

    fix naming conventions

--- src/test/java/integration/LocalisationTestIT.java
+++ src/test/java/integration/LocalisationTestIT.java
@@ -12,38 +12,38 @@ import org.testng.annotations.Test;
 
 public class LocalisationTestIT {
 
-    private Sylenium $y;
+    private Sylenium sy;
 
     @BeforeTest
     public void clearProperties() {
         System.clearProperty("sylenium.language.file");
-        $y = Sylenium.getInstance();
+        sy = Sylenium.getInstance();
     }
 
     @Test(expectedExceptions = NoSuchLanguageFileException.class)
     public void noLocalisationFileThrowsNoSuchLanguageFileException() {
         System.setProperty("sylenium.language.file", "made-up.properties");
-        $y.localised("fail");
+        sy.localised("fail");
     }
 
     @Test
     public void validLocalisationFileCanReadProperty() {
         System.setProperty("sylenium.language.file", "english.properties");
-        Assert.assertEquals($y.localised("one"), "1");
+        Assert.assertEquals(sy.localised("one"), "1");
     }
 
     @Test
     public void runtimeChangesToLanguageFileAreAccurate() {
         System.setProperty("sylenium.language.file", "english.properties");
-        Assert.assertEquals($y.localised("one"), "1");
+        Assert.assertEquals(sy.localised("one"), "1");
         System.setProperty("sylenium.language.file", "jibberish.properties");
-        Assert.assertEquals($y.localised("tango"), "orange");
+        Assert.assertEquals(sy.localised("tango"), "orange");
     }
 
     @Test(expectedExceptions = NoSuchLocalisedPropertyException.class)
     public void noSuchPropertyThrowsNoSuchLocalisedPropertyException() {
         System.setProperty("sylenium.language.file", "english.properties");
-        Assert.assertEquals($y.localised("n0p3"), "1");
+        Assert.assertEquals(sy.localised("n0p3"), "1");
     }
 
 }
