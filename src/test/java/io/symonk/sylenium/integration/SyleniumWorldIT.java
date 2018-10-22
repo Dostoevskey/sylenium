2f48d83f15b54ea368c120564b8b14dd3f6cc248
simonk <sek_99@hotmail.co.uk>
2018-10-22 16:48:44 +0100

    commit missing tests

--- src/test/java/io/symonk/sylenium/integration/SyleniumWorldIT.java
+++ src/test/java/io/symonk/sylenium/integration/SyleniumWorldIT.java
@@ -3,6 +3,7 @@ package io.symonk.sylenium.integration;
 import io.symonk.sylenium.Sylenium;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 
 public class SyleniumWorldIT {
@@ -14,11 +15,13 @@ public class SyleniumWorldIT {
         sy = new Sylenium();
     }
 
+    @Test
     public void registerWorldObject() {
         sy.register(new DummyWorldObject());
         Assert.assertEquals(sy.getWorldSize(), 1);
     }
 
+    @Test
     public void canCleanUpTheWorld() {
         sy.register(new DummyWorldObject());
         sy.cleanUpWorld();
