ad38de25ac27ec91ecef661825165483d97b9abe
simonk <sek_99@hotmail.co.uk>
2018-10-22 17:30:22 +0100

    remove unused var

--- src/main/java/io/symonk/sylenium/SyleniumWorld.java
+++ src/main/java/io/symonk/sylenium/SyleniumWorld.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class SyleniumWorld<T extends SyleniumObject> {
 
   private List<T> testObjects = new ArrayList<>();
-  private int numberOfObjects;
 
   public void registerObject(T t) {
     testObjects.add(t);
