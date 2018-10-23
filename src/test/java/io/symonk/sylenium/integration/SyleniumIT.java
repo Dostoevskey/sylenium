de007050400fb5a7045336511254653d9ae9b2b0
simonk <sek_99@hotmail.co.uk>
2018-10-23 18:58:41 +0100

    remove duplicate deps, remove @ignore, fix tests

--- src/test/java/io/symonk/sylenium/integration/SyleniumIT.java
+++ src/test/java/io/symonk/sylenium/integration/SyleniumIT.java
@@ -2,13 +2,11 @@ package io.symonk.sylenium.integration;
 
 import io.symonk.sylenium.SyleniumTest;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class SyleniumIT extends SyleniumTest {
 
-  @Test()
-  @Ignore
+  @Test
   public void startingWithPoInitialisesPage() {
     final DummyWorldObject po = sylenium.start("https://www.google.com", DummyWorldObject.class);
     Assert.assertNotNull(po);
