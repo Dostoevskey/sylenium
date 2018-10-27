4abc84f5c6c4cabc9a9eb012847d73c0706d896f
simonk <sek_99@hotmail.co.uk>
2018-10-27 12:14:34 +0100

    more test coverage on link text lookups

--- src/test/java/io/symonk/sylenium/integration/SyleniumLinkTextIT.java
+++ src/test/java/io/symonk/sylenium/integration/SyleniumLinkTextIT.java
@@ -9,13 +9,26 @@ import org.testng.annotations.Test;
 public class SyleniumLinkTextIT extends SyleniumTest {
 
     @Test
-    public void canFindPartialTextElementByLocalisedLookup() {
+    public void canFindExactLinkTextElement() {
         sylenium.launch("http://toolsqa.com/automation-practice-form/", DummyWorldObject.class);
-        sylenium.$localisedLinkText("partial.link.text").shouldBe(Condition.visible);
+        sylenium.$localisedLinkText("link.text").shouldBe(Condition.visible);
     }
 
     @Test
-    public void canFindPartialTextElementsByLocalisedLookup() {
+    public void canFindListOfExactTextElements() {
+        sylenium.launch("http://toolsqa.com/automation-practice-form/", DummyWorldObject.class);
+        sylenium.$$localisedLinkText("link.text").shouldHaveSize(1);
+    }
+
+
+    @Test
+    public void canFindPartialLinkTextElement() {
+        sylenium.launch("http://toolsqa.com/automation-practice-form/", DummyWorldObject.class);
+        sylenium.$localisedPartialLinkText("multiple.link.text").shouldBe(Condition.visible);
+    }
+
+    @Test
+    public void canFindListOfPartialTextElements() {
         sylenium.launch("http://toolsqa.com/automation-practice-form/", DummyWorldObject.class);
         sylenium.$$localisedPartialLinkText("multiple.link.text").shouldHaveSize(11);
     }
