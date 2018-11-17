74856379854af7451f0ce4e096af120ede35dc4b
$y <jackofspaces@gmail.com>
2018-11-17 21:43:15 +0000

    add guava dependency, make adjustments to Sylenium world

--- src/main/java/io/symonk/sylenium/SyleniumWorld.java
+++ src/main/java/io/symonk/sylenium/SyleniumWorld.java
@@ -1,16 +1,23 @@
 package io.symonk.sylenium;
 
+import com.google.common.collect.Lists;
 import io.symonk.sylenium.contracts.SyleniumObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Sylenium World is a place for test data to live and to be persisted cross-suite if necessary.
+ * It is a generic implementation and can keep track of anything you need, cleaning it up based on a clean up strategy
+ * @param <T> A piece of Testdata that implements the SyleniumObject interface
+ */
 public class SyleniumWorld<T extends SyleniumObject> {
 
   private List<T> testObjects = new ArrayList<>();
 
-  public void registerObject(T t) {
+  public T registerObject(T t) {
     testObjects.add(t);
+    return t;
   }
 
   public void unregisterObject(T t) {
@@ -21,8 +28,12 @@ public class SyleniumWorld<T extends SyleniumObject> {
     return testObjects.size();
   }
 
+  /**
+   * Clean up the items in reverse order, a child instance can exist before its parent so it makes logical sense
+   * to flush out the world in reverse order to avoid issues where a parent cannot be deleted because it has children.
+   */
   public void cleanUpWorld() {
-    testObjects.forEach(SyleniumObject::cleanUp);
+    Lists.reverse(testObjects).forEach(testData -> testData.cleanUp());
     testObjects.clear();
   }
 }
